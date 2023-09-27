c42773ebe8c9962b63c35e7b064cc639ccddae0e
Marek Wenta <marek.wenta2009@gmail.com>
2023-09-27 14:44:49 +0200

    c gjg9s9gj9jv9fo

---
+++ main.py
@@ -0,0 +1,122 @@
+#Zmienne
+
+liczba1 = 10
+print(type(liczba1))
+
+tekst1 = "kocham Kraczaja"
+print(type(tekst1))
+
+log1 = True
+print(type(log1))
+
+liczba1 = "xd"
+print(type(liczba1))
+
+liczba2 = 2.67
+print(type(liczba2))
+
+lista1 = [ 'kot', 12, False, 3.89]
+
+print(lista1)
+
+
+zbior1 = {1, 1, 1, 2, 2, 2, 2, 3, 5, 6}
+print(type(zbior1))
+
+slownik1 = {"kot":"spada na 4 łapy", "pies" : "szczeka", "kogut" : "pieje"}
+print(slownik1["kot"])
+
+#Wypisywanie danych na ekranie
+liczba3 = 575
+print(liczba3)
+
+print(5 + 7)
+
+print('ruchanie')
+
+print(True)
+
+#Wczytywanie danych od użytkownika
+liczba4 = input(' Podaj liczbę')
+print(type(liczba4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
